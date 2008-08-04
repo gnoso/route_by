fec659d187b9478997acf0cdbfa802a389027ba6
Peter Waldschmidt <peterw@gnoso.com>
2008-08-03 23:38:02 -0400

    Initial Commit

---
+++ lib/route_by.rb
@@ -0,0 +1,41 @@
+# RouteBy
+module RouteBy
+
+  def self.included(cls)
+    cls.extend(ClassMethods)
+  end
+
+  module ClassMethods
+
+    # this enables "route-by" functionality for an ActiveRecord model class
+    def route_by(field_name)
+      
+      # attach the instance methods to this class
+      include InstanceMethods
+      
+      # add validation for the id
+      validates_presence_of :name
+      validates_uniqueness_of :name
+      validates_format_of :name, :with => /\A[^\/\. ]*\Z/, :message => "may not contain the characters '/', '.' or ' '"
+      
+      # set the field_name into a class instance variable
+      self.instance_variable_set(:@route_by_field_name, field_name)
+    end
+    
+    # lookup the object by the given field_name
+    def find_by_route_id(id)
+      self.send("find_by_#{self.instance_variable_get(:@route_by_field_name)}", id)
+    end
+
+  end
+
+  module InstanceMethods
+    
+    # this allows our urls to be based on the name rather than the id
+    def to_param
+      send(self.class.instance_variable_get(:@route_by_field_name))
+    end
+
+  end
+  
+end
